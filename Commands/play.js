f429365f507321a1484555713c9f0b24fd4217d7
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-13 09:47:17 +0530

    Update play.js

--- Commands/play.js
+++ Commands/play.js
@@ -4,20 +4,70 @@ exports.run = async (client, message, args, ops) => {
     
     if (!message.member.voice.channel) return message.channel.send('Please connect to a voice channel...');
     
-    if (message.guild.me.voice.channel) return message.channel.send('The bot is already connected to the guild...');
-    
     if (!args[0]) return message.channel.send('Please input a URL following the command...');
 
     let validate = await ytdl.validateURL(args[0]);
 
     if (!validate) return message.channel.send('Sorry, please enter a valid URL...');
+//#region 
+    // let info = await ytdl.getInfo(args[0]);
+
+    // let connection = await message.member.voice.channel.join();
+
+    // let dispatcher = await connection.play(ytdl(args[0], { filter: 'audioonly' }));
+
+    // message.channel.send(`Now playing: ${info.title}`);
+//#endregion
 
     let info = await ytdl.getInfo(args[0]);
+    
+    let data = ops.active.get(message.guild.id) || {};  //fetch active and if not defined, define with {}
+    if (!data.connection) data.connection = await message.member.voice.channel.join();      //make connection, if there is not
+    if (!data.queue) data.queue = [];       //make a queue if it doesn't exists
+    data.guildID = message.guild.id;
 
-    let connection = await message.member.voice.channel.join();
+    //adding to queue
+    data.queue.push({
+        songTitle: info.title,
+        requester: message.author.tag,
+        url: args[0],
+        announceChannel: message.channel.id
+    });
 
-    let dispatcher = await connection.play(ytdl(args[0], { filter: 'audioonly' }));
+    if (!data.dispatcher) play(client, ops, data);
+    else {
+        message.channel.send(`Added to queue: ${info.title} | Requested by: ${message.author.id}`);
+    }
 
-    message.channel.send(`Now playing: ${info.title}`);
+    //update map
+    ops.active.set(message.guild.id, data);
 
+}
+
+async function play(client, ops, data) {
+    client.channels.get(data.queue[0].announceChannel).send(`Now Playing: ${data.queue[0].songTitle} | Requested by: ${data.queue[0].requester}`);
+
+    data.dispatcher = await data.connection.play(ytdl(data.queue[0].url, { filter: 'audioonly' }));
+    data.dispatcher.guild.id = data.guildID;
+
+    //song end
+    data.dispatcher.once('finish', function () {
+        finish(client, ops, this);
+    });
+
+}
+
+function finish(client, ops, dispatcher) {
+    let fetched = ops.active.get(dispatcher.guildID);
+    fetched.queue.shift();
+
+    if (fetched.queue.length > 0) {
+        ops.active.set(dispatcher.guildID, fetched);
+        play(client, ops, fetched);
+    } else {
+        ops.active.delete(dispatcher.guildID);
+
+        let voiceChannel = client.guilds.get(dispatcher.guildID).me.voice.channel;
+        if (voiceChannel) voiceChannel.leave();
+    }
 }
