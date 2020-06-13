d103d3b77c313dda05f0e9c2c494e9627b327b72
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-13 14:13:14 +0530

    added stop

--- Commands/stop.js
+++ Commands/stop.js
@@ -1,17 +1,16 @@
 exports.run = async (client, message, args, ops) => {
+
     let fetched = ops.active.get(message.guild.id);
 
     if (!fetched) return message.channel.send('There isn\'t currently any music playing here...');
 
     if (message.member.voice.channel !== message.guild.me.voice.channel) return message.channel.send('Sorry, you currently aren\'t in the same voice channel...');
 
-    let queue = fetched.queue;
-    let nowPlaying = queue[0];
+    if (!fetched.dispatcher.paused)
+        fetched.dispatcher.pause();
+    
+    fetched.queue.length = 0;
 
-    if (!queue[1]) return message.channel.send('Nothing in queue to clear...');
-    else {
-        fetched.queue.length = 1;
-        return message.channel.send('Queue cleared...');
-    }
-
+    message.guild.me.voice.channel.leave();
+    message.channel.send('Stopped...');
 }
