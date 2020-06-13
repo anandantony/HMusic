e48f6131fa5d77a0c7a06b4243e486f8f95ced73
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-13 11:05:52 +0530

    Create queue.js

---
+++ Commands/queue.js
@@ -0,0 +1,17 @@
+exports.run = async (client, message, args, ops) => {
+    let fetched = ops.active.get(message.guild.id);
+
+    if (!fetched) return message.channel.send('There isn\'t currently any music playing here...');
+
+    let queue = fetched.queue;
+    let nowPlaying = queue[0];
+
+    let resp = `__**Now Playing**__\n**${nowPlaying.songTitle}** -- **Requested by:** *${nowPlaying.requester}*\n\n__**Queue**__\n`;
+
+    for (var i = 1; i < queue.length; i++) {
+        resp += `${i}. **${queue[i].songTitle}** -- **Requested By:** *${queue[i].requester}*\n`;
+    }
+
+    message.channel.send(resp);
+
+}
