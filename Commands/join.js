58f615733a6edd2f1c764ef4e7ef54201f8e8d1a
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-13 15:06:59 +0530

    Create join.js

---
+++ Commands/join.js
@@ -0,0 +1,5 @@
+exports.run = (client, message, args, ops) => {
+    if (message.member.voice.channel !== message.guild.me.voice.channel) {
+        await message.member.voice.channel.join();
+    }
+}
