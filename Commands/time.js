885b08b44a8b9c889949773169af5b92e1acc775
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-13 13:48:57 +0530

    Update time.js

--- Commands/time.js
+++ Commands/time.js
@@ -1,7 +1,23 @@
 exports.run = (client, message, args) => {
     var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
+    var offset = date.getTimezoneOffset();
+    var hourOffset;
+    var minuteOffset;
+    if (offset == 0) {
+        hourOffset = 0;
+        minuteOffset = 0;
+    }
+    else if (offset > 0) {
+        hourOffset = -(offset / 60);
+        minuteOffset = -(offset % 60);
+    }
+    else {
+        hourOffset = (offset / 60);
+        minuteOffset = (offset % 60);
+    }
+        
+    var hours = date.getHours() + hourOffset;
+    var minutes = date.getMinutes() + minuteOffset;
     var ampm = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
