cbf60ca5a0be30f5128d732560b74bd0779d0b33
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-13 14:51:33 +0530

    Update queue.js

--- Commands/queue.js
+++ Commands/queue.js
@@ -1,4 +1,6 @@
 exports.run = async (client, message, args, ops) => {
+    if (!message.member.voice.channel) return message.channel.send('Please connect to a voice channel...');
+    
     let fetched = ops.active.get(message.guild.id);
 
     if (!fetched) return message.channel.send('There isn\'t currently any music playing here...');
