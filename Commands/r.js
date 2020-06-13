e727ec981c86c260b123237674df43ddebb9f5b9
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-13 19:23:45 +0530

    Shortcut commands

---
+++ Commands/r.js
@@ -0,0 +1,16 @@
+exports.run = async (client, message, args, ops) => { 
+
+    let fetched = ops.active.get(message.guild.id);
+
+    if (!fetched) return message.channel.send('There isn\'t currently any music playing here...');
+
+    if (message.member.voice.channel !== message.guild.me.voice.channel) return message.channel.send('Sorry, you currently aren\'t in the same voice channel...');
+
+    if (!fetched.dispatcher.paused) return message.channel.send('This music isn\'t paused...');
+
+    fetched.dispatcher.resume();
+
+    if (fetched.queue[0])
+        message.channel.send(`Successfully resumed ${fetched.queue[0].songTitle}`);
+
+}
