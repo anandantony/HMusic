ca34b35548e52338861645bed2e40b80ff7e3072
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-13 14:54:25 +0530

    Update queue.js

--- Commands/queue.js
+++ Commands/queue.js
@@ -1,6 +1,8 @@
 exports.run = async (client, message, args, ops) => {
+    if (!message.guild.me.voice.channel) return message.channel.send('Sorry, the bot isn\'t connected to the guild...');
+
     if (!message.member.voice.channel) return message.channel.send('Please connect to a voice channel...');
-    
+
     let fetched = ops.active.get(message.guild.id);
 
     if (!fetched) return message.channel.send('There isn\'t currently any music playing here...');
