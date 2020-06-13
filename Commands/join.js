ad5ca55faef6d592635bbbfd0181b3dfddcb45f3
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-13 15:29:11 +0530

    Update join.js

--- Commands/join.js
+++ Commands/join.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args, ops) => {
     if (message.member.voice.channel !== message.guild.me.voice.channel) {
-        await message.member.voice.channel.join();
+        ops.active.connection = await message.member.voice.channel.join();
     }
 }
