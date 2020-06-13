4f381c948c6b3d18a7a8440e739da68d8684cc91
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-13 15:54:46 +0530

    renamed leave to stop, updated help

--- Commands/join.js
+++ Commands/join.js
@@ -1,6 +1,7 @@
 exports.run = async (client, message, args, ops) => {
     if (message.member.voice.channel !== message.guild.me.voice.channel) {
         ops.active.connection = await message.member.voice.channel.join();
+        message.channel.send('Joined...');
     }
     else if (message.member.voice.channel === message.guild.me.voice.channel) {
         message.channel.send('Already joined...');
