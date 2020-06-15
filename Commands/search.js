f95cb8be9e2b08f691458d938f25498c157a6e40
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-15 21:43:31 +0530

    Update search.js

--- Commands/search.js
+++ Commands/search.js
@@ -16,11 +16,15 @@ exports.run = async (client, message, args, ops) => {
 
         message.channel.send(resp);
 
-        const filter = m => !isNaN(m.content) && m.content < videos.length + 1 && m.content > 0 && m.content == 'c';
+        // const filter = m => !isNaN(m.content) && m.content < videos.length + 1 && m.content > 0 && m.content == 'c';
+        const filter = m => (m.content < videos.length + 1 && m.content > 0) || m.content == 'c';
         const collector = message.channel.createMessageCollector(filter, { max: 1, maxProcessed: 1 });
 
         try {
             collector.once('collect', function (m) {
+                if (m.content === 'c') {
+                    return message.channel.send('Search cancelled...');
+                }
                 let commandFile = require(`./play`);
                 commandFile.run(client, message, [videos[parseInt(m.content) - 1].url], ops);
             });
