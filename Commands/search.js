c539267dd0109732c51433756d658d95de90b349
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-15 21:55:24 +0530

    Update search.js

--- Commands/search.js
+++ Commands/search.js
@@ -12,19 +12,15 @@ exports.run = async (client, message, args, ops) => {
             resp += `**[${parseInt(i) + 1}]:** \`${videos[i].title}\`\n`;
         }
 
-        resp += `\n**Choose a number between \`1-${videos.length}\` or enter c to cancel search**`;
+        resp += `\n**Choose a number between \`1-${videos.length}\` , any other entries will be ignored.**`;
 
         message.channel.send(resp);
 
-        // const filter = m => !isNaN(m.content) && m.content < videos.length + 1 && m.content > 0 && m.content == 'c';
-        const filter = m => (m.content < videos.length + 1 && m.content > 0) || m.content == 'c';
+        const filter = m => !isNaN(m.content) && m.content < videos.length + 1 && m.content > 0;
         const collector = message.channel.createMessageCollector(filter, { max: 2, maxProcessed: 2 });
 
         try {
             collector.once('collect', function (m) {
-                if (m.content === 'c') {
-                    return message.channel.send('Search cancelled...');
-                }
                 let commandFile = require(`./play`);
                 commandFile.run(client, message, [videos[parseInt(m.content) - 1].url], ops);
             });
