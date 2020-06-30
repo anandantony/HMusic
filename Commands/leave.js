1d884c01594bd327afd970a071b40006fd306d10
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-30 13:06:19 +0530

    Update leave.js

--- Commands/leave.js
+++ Commands/leave.js
@@ -2,6 +2,13 @@ exports.run = (client, message, args, ops) => {
     if (!message.member.voice.channel) return message.channel.send('Please connect to a voice channel..');
 
     if (!message.guild.me.voice.channel) return message.channel.send('Sorry, the bot isn\'t connected to the guild...');
+    let fetched = ops.active.get(message.guild.id);
+    
+
+    if (fetched) {
+        fetched.dispatcher.end();
+        ops.active.queue = [];
+    }
     message.guild.me.voice.channel.leave();
     message.channel.send('Leaving channel...');
 }
