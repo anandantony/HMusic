a10c62a14188a45850507ab6e3b4c086dbbe3f03
anandantony <42650677+anandantony@users.noreply.github.com>
2020-06-30 12:31:40 +0530

    Update play1.js

--- Commands/play1.js
+++ Commands/play1.js
@@ -3,7 +3,7 @@ const ytsearch = require('yt-search');
 exports.run = async (client, message, args, ops) => { 
     
     ytsearch(args.join(' '), function (err, result) {
-        if (err) return message.channel.send('Sorry, something went wrong...');
+        if (err) return message.channel.send('Sorry, conection time out...');
 
         let commandFile = require(`./play`);
         commandFile.run(client, message, [result.videos[0].url], ops);
