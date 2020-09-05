38068f54e4e06d46c7bbb3ed81fcd73278f2b4cc
anandantony <42650677+anandantony@users.noreply.github.com>
2020-09-05 16:54:48 +0530

    Update play.js

--- Commands/play.js
+++ Commands/play.js
@@ -44,7 +44,7 @@ exports.run = async (client, message, args, ops) => {
 async function play(client, ops, data) {
     client.channels.cache.get(data.queue[0].announceChannel).send(`Now Playing: ${data.queue[0].songTitle} | Requested by: ${data.queue[0].requester}`);
 
-    if(message.member.guild == client.guild){
+    if(client.channels.cache.get(data.queue[0].announceChannel).guild == client.guild){
         client.user.setPresence({
         activity: {
             name: `${data.queue[0].songTitle}`,
