79aee968fa26a2b1e468bf2b7b21d4d12ae0c572
anandantony <42650677+anandantony@users.noreply.github.com>
2020-10-31 16:48:55 +0530

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -1,4 +1,11 @@
 exports.run = async (client, message, args, ops) => { 
-    resp = `**List of Commands:\n1. -play : input a URL or the name of the song after the command.\n2. -search : Searches for a song and provides a list to choose from.\n3. -p : Pauses or Resumes the song.\n4. -skip : Skips the current song.\n5. -v / -volume : Sets the volume of the song.\n6. -q / -queue : Shows the current queue.\n7. -clear : Clears the queue.\n8. -stop : Stops and leaves the voice channel**`;
+    resp = `**List of Commands:\n1. -play : input a URL or the name of the song after the command. If a song was paused it resumes the song.
+    \n2. -search : Searches for a song and provides a list to choose from.
+    \n3. -p : Pauses or Resumes the song.
+    \n4. -skip : Skips the current song.
+    \n5. -v / -volume : Sets the volume of the song.
+    \n6. -q / -queue : Shows the current queue.
+    \n7. -clear : Clears the queue.
+    \n8. -stop : Stops and leaves the voice channel**`;
     message.channel.send(resp);
 }
